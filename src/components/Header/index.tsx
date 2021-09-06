e1eefadf6249f133c6207d5b3f73382a388807c5
Matheus Massari <massari.matheus@gmail.com>
2021-09-06 11:40:16 -0300

    adicionadas novas dependencias: axios, miragejs, react-modal. - iniciando edicao do modal da pagina

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,26 @@
-import logoImg from '../../assets/logo.svg'
-import { Container, Content } from './styles'
+import { useState } from 'react';
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+import Modal from 'react-modal';
 
-console.log(logoImg)
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-export function Header() {
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+    
+
     return (
       <Container>
         <Content>
           <img src={logoImg} alt="dt money" />
-          <button type="button">Nova transação</button>
+          <button 
+          type="button" 
+          onClick={onOpenNewTransactionModal}
+          >
+            Nova transação
+          </button>
+          
         </Content>
       </Container>
     );
